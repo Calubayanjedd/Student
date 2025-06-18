0ca0984c7fb245123e4009368c9fd97379e3509b
Ruther Calubayan <rcalubayan@juntec.com.ph>
2025-06-18 16:24:35 +0800

    add a comment

--- LoginForm.vb
+++ LoginForm.vb
@@ -6,4 +6,6 @@
         Me.Hide() ' Optional: hides the login form
     End Sub
 
+    ' in this form you should create a log in information for anyone, since starting palang naman kayo so kahit yung admin tas admin123 lang na pass, no need na ng database to AHAHAHAHAHHA
+
 End Class
